1c4884f8805f3c530f8979fd4bcc5b0300e9366c
Randy2222222 <randyjpollitt@yahoo.com>
2026-02-09 16:01:27 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    let cartLink = document.getElementById('cartLink');
-    if (cartLink) {
-        cartLink.textContent = `My Cart (${cart.length})`;
-    }
-});
